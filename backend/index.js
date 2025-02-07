45a041c3ad9c1a172e6d343c6645f37b13279878
xArsenic <76486130+xYeshu@users.noreply.github.com>
2025-02-07 22:58:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -151,7 +151,7 @@ app.put("/api/chats/:id", requireAuth(), async (req, res) => {
 
 
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   connect()
   console.log('Live at Port 3000');
 });
