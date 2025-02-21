53d306d8d817a60a1325c920d2d4b9dcc23e57f0
xArsenic <76486130+xYeshu@users.noreply.github.com>
2025-02-22 00:27:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,10 @@ const app = express()
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"], 
     credentials: true,
-  }));
+  })
+);
 
 app.use(express.json())
 
